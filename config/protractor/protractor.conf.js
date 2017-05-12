12a0083002b35ff73aae4cd254758285d09db556
BLACKBAUD\Trevor.Burch <Trevor.Burch@blackbaud.com>
2017-05-12 18:03:30 -0400

    Add reporter to give a visual report of e2e tests

--- config/protractor/protractor.conf.js
+++ config/protractor/protractor.conf.js
@@ -2,7 +2,9 @@
 'use strict';
 
 const path = require('path');
-const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+const SpecReporter = require('jasmine-spec-reporter');
+var jasmineReporters = require('jasmine-reporters');
+var fs = require('fs-extra');
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -34,5 +36,51 @@ exports.config = {
 
   onPrepare: function () {
     jasmine.getEnv().addReporter(new SpecReporter());
-  }
+    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+        consolidateAll: true,
+        savePath: './',
+        filePrefix: 'xmlresults'
+    }));
+    fs.emptyDir('screenshots/', function (err) {
+        console.log(err);
+    });
+
+    jasmine.getEnv().addReporter({
+        specDone: function(result) {
+            if (result.status == 'failed') {
+                browser.getCapabilities().then(function (caps) {
+                    var browserName = caps.get('browserName');
+
+                    browser.takeScreenshot().then(function (png) {
+                        var stream = fs.createWriteStream('screenshots/' + browserName + '-' + result.fullName+ '.png');
+                        stream.write(new Buffer(png, 'base64'));
+                        stream.end();
+                    });
+                });
+            }
+        }
+    });
+  },
+  onComplete: function() {
+     var browserName, browserVersion;
+     var capsPromise = browser.getCapabilities();
+
+     capsPromise.then(function (caps) {
+        browserName = caps.get('browserName');
+        browserVersion = caps.get('version');
+
+        var HTMLReport = require('protractor-html-reporter');
+
+        var testConfig = {
+            reportTitle: 'Test Execution Report',
+            outputPath: './',
+            screenshotPath: './screenshots',
+            testBrowser: browserName,
+            browserVersion: browserVersion,
+            modifiedSuiteName: false,
+            screenshotsOnlyOnFailure: true
+        };
+        new HTMLReport().from('xmlresults.xml', testConfig);
+    });
+ }
 };
